5f006f310521ac47bfbc37a66cd9db52b6216ddc
fortune-c <fortunate.adesinas@gmail.com>
2025-12-24 20:03:15 +0100

    initial setup to intial deployment

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <x-slot:title>Home</x-slot:title>
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">Welcome to Chipper!</h1>
+                    <p class="mt-4 text-base-content/60">Share your moments. Post your Chips. Connect instantly. No clutter, no hassle—just Chipper.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
